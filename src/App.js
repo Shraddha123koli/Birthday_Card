d1c42540606d867a32b903c1c9aae52795200448
Birthday_card <kshraddha738@gmail.com>
2023-10-30 14:01:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Data from './Data';
+// import './Project1_ProfileCard/Data.css';
+import './App.css'
 
-function App() {
+function App(){
+  const[data,setData]=useState(Data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='App'>
+      <section className="sec1">
+      <main className="main1">
+         <p>Today Birthday {data.length}😍</p>
+          {data.map((x)=>{
+            return(
+              <div className='Cards' key={x.id}>
+                <img src={x.image} alt="" height={'50px'} width={'50px'}/>
+                <ruby><p>{x.age}</p><rt>{x.name}</rt></ruby>
+              </div>
+            );
+          })
+          }
+          <button id="btn1" onClick={()=>setData([])}>Clear</button>
+        </main>
+        
+      </section> 
+    </div> 
+  )
 }
-
-export default App;
+export default App
---
+++ src/Data.js
@@ -0,0 +1,27 @@
+const Data=[
+    {
+        id:10,name:"Shah Rukh Khan", age:"57",
+        image:"https://th.bing.com/th/id/OIP.fmajKf-pDUhXRAdI8TsApgHaEz?pid=ImgDet&rs=1"
+    },
+    {
+        id:20,name:"Anushka Sharma", age:"31",
+        image:"https://images.hindustantimes.com/rf/image_size_640x362/HT/p2/2016/07/11/Pictures/anushka-sharma-at-trailer-launch-of-sultan_34c742e4-4735-11e6-a5ed-4b8bf40e703f.jpg"
+    },
+    {
+        id:1,name:"Virat Kohli", age:"35",
+        image:"https://i.pinimg.com/originals/f0/20/db/f020db0277a4759bdbe67df6911d0b01.jpg"
+    },
+    {
+        id:12,name:"Shiv Thakare", age:"29",
+        image:"https://staticimg.amarujala.com/assets/images/2023/03/30/750x506/shiv-thakare-shiv-thakare-on-casting-couch-shiv-thakare-casting-couch-shiv-thakare-bigg-boss-shi_1680152480.jpeg"
+    },
+    {
+        id:13,name:"Aliya Bhatt", age:"30",
+        image:"https://i.pinimg.com/736x/7e/f3/d6/7ef3d6aedf0c94cf373dbadaccd79f66.jpg"
+    },
+    {
+        id:14,name:"Rohit Sharma", age:"32",
+        image:"https://media.fashionnetwork.com/cdn-cgi/image/fit=contain,width=1000,height=1000/m/1eff/dc85/54f6/c7c6/494f/c45a/941e/a244/841b/355c/355c.jpeg"
+    }
+]
+export default Data
